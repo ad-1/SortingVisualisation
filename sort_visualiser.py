d053b9785acbb2d7ea7b57e5235e1d42a1787da0
AndyJ <andrewdaviesul@gmail.com>
2020-02-27 15:37:37 +0000

    simplified ttk code

--- sort_visualiser.py
+++ sort_visualiser.py
@@ -1,12 +1,11 @@
 # Sorting Algorithm Visualiser
 
-import time
 from bar import Bar
 import tkinter as tk
 from ttkthemes import themed_tk as theme
 from tkinter import ttk as tkk
 from random import sample
-from algorithms.solver import Solver
+from solver import Solver
 
 
 class Visualiser:
@@ -18,7 +17,7 @@ class Visualiser:
         self.root = theme.ThemedTk()
         self.menu_frame = tkk.Frame(self.root, width=self.graph_width)
         self.canvas = tk.Canvas(self.root, height=self.graph_height, width=self.graph_width, bg='#424242')
-        self.n_bars = 100
+        self.n_bars = 1000
         self.bars = [None for _ in range(0, self.n_bars)]
         self.bar_width = self.graph_width / self.n_bars
         self.y_scale = 0
@@ -53,6 +52,28 @@ class Visualiser:
             self.bars[i] = bar
             self.render_bar(bar)
 
+    def clean_canvas(self):
+        """ delete all bars from the canvas and redraw """
+        for bar in self.bars:
+            self.canvas.delete(bar.shape)
+
+    def change_array_size(self, val):
+        """ change graph size """
+        self.n_bars = int(float(val))
+        self.clean_canvas()
+
+    def algorithm_change(self, *args):
+        """ change solve mode """
+        self.solve_mode = self.algorithms.index(args[0])
+
+    def validate_setup(self):
+        """ validate inputs to sort """
+        if self.bars is None:
+            return
+        self.is_solving = True
+        Solver(self.bars, self.n_bars, self.solve_mode, self)
+        self.is_solving = False
+
     def render_bar(self, bar):
         """ draw bar on canvas """
         bar.shape = self.canvas.create_rectangle(bar.x1, 0, bar.x2, bar.value * self.y_scale, fill='#2F7AE5')
@@ -68,13 +89,13 @@ class Visualiser:
     def config_menu(self):
         """ menu - user configurable settings for visualisation"""
         self.menu_frame.grid(row=0, column=0, sticky='n')
-        self.tkk_btn(self.menu_frame, [('Visualise', self.validate_setup)], True)
+        tkk.Button(self.menu_frame, text='Visualise', command=self.validate_setup)
         current_algo = tk.StringVar()
         current_algo.set(self.algorithms[self.solve_mode])
         algorithms_menu = tkk.OptionMenu(self.menu_frame, current_algo, self.algorithms[0], *self.algorithms, command=self.algorithm_change)
         algorithms_menu.config(width=15)
-        self.tkk_label(self.menu_frame, ['Array Size'])
-        self.tkk_scaler(100, 350, self.change_array_size)
+        tkk.Label(self.menu_frame, text='Array Size', anchor='e')
+        tkk.Scale(self.menu_frame, from_=100, to=350, orient=tk.HORIZONTAL, command=self.change_array_size)
         for r, child in enumerate(self.menu_frame.winfo_children()):
             pad = 0 if isinstance(child, tk.Button) else 5
             child.grid_configure(row=0, column=r, sticky='ew', padx=pad, pady=pad)
@@ -84,39 +105,3 @@ class Visualiser:
         kp = repr(event.keysym)
         if kp == '\'v\'':
             self.validate_setup()
-
-    def change_array_size(self, val):
-        """ change graph size """
-        self.n_bars = float(val)
-        self.config_bars()
-
-    @staticmethod
-    def tkk_btn(parent, btns, is_tkk):
-        """ create buttons given list of text and commands """
-        for text, cmd in btns:
-            if is_tkk:
-                tkk.Button(parent, text=text, command=cmd)
-            else:
-                tk.Button(parent, text=text, highlightbackground=cmd, height=1, state='disabled')
-
-    @staticmethod
-    def tkk_label(parent, text):
-        """ create labels from list of text """
-        for t in text:
-            tkk.Label(parent, text=t, anchor='e')
-
-    def tkk_scaler(self, from_, to_, cmd):
-        """ create themed scaler """
-        tkk.Scale(self.menu_frame, from_=from_, to=to_, orient=tk.HORIZONTAL, command=cmd)
-
-    def algorithm_change(self, *args):
-        """ change solve mode """
-        self.solve_mode = self.algorithms.index(args[0])
-
-    def validate_setup(self):
-        """ validate inputs to sort """
-        if self.bars is None:
-            return
-        self.is_solving = True
-        Solver(self.bars, self.n_bars, self.solve_mode, self)
-        self.is_solving = False
